448cadd980cda32c82e5d7508028616b6ffcbb6e
Christophe Denutte <christophe.denutte@live.be>
2022-04-02 16:17:29 +0200

    Add project files.

---
+++ NutzWeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace NutzWeb.Models
+{
+    // 000 - Create Project
+    public class Category
+    {
+        public int Id { get; set; } // Id
+        public string Name { get; set; } // Name
+        public int DisplayOrder { get; set; } // DisplayOrder 
+        public DateTime CreatedateTime { get; set; } = DateTime.Now; // CreatedateTime
+
+    }
+}
