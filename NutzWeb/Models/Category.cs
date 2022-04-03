fa5d2cfa619d03a384b02dd7e16ebc26b2747d62
Christophe Denutte <christophe.denutte@live.be>
2022-04-03 10:39:32 +0200

    Create Project

--- NutzWeb/Models/Category.cs
+++ NutzWeb/Models/Category.cs
@@ -1,9 +1,13 @@
-﻿namespace NutzWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NutzWeb.Models
 {
     // 000 - Create Project
     public class Category
     {
+        [Key]
         public int Id { get; set; } // Id
+        [Required]
         public string Name { get; set; } // Name
         public int DisplayOrder { get; set; } // DisplayOrder 
         public DateTime CreatedateTime { get; set; } = DateTime.Now; // CreatedateTime
