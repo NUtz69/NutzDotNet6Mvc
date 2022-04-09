557d4fc65fb1d935e9f79b8c05191628555f9bfa
Christophe Denutte <christophe.denutte@live.be>
2022-04-09 16:17:12 +0200

    Create Project DCWeb

---
+++ DCWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DCWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DCWeb.Data
+{
+    // 002 - Create Project DCWeb
+    public class ApplicationDbContext : DbContext
+    {
+        // ctor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        // DbSet
+        public DbSet<Category>? Category { get; set; }
+    }
+}
